06f83b9d668d4a1c4f19b22dc882c4b2bfdda4bf
wahnaton <wilbur777@protonmail.com>
2025-08-17 09:33:03 -0700

    Define fourthwall service interface

--- src/modules/fourthwall/index.ts
+++ src/modules/fourthwall/index.ts
@@ -1,4 +1,4 @@
-import FourthwallModuleService from "./service"
+import FourthwallModuleService, { IFourthwallModuleService } from "./service"
 import { Module } from "@medusajs/utils"
 
 export const FOURTHWALL_MODULE = "fourthwall"
@@ -6,3 +6,5 @@ export const FOURTHWALL_MODULE = "fourthwall"
 export default Module(FOURTHWALL_MODULE, {
   service: FourthwallModuleService,
 })
+
+export type { IFourthwallModuleService }
--- src/modules/fourthwall/service.ts
+++ src/modules/fourthwall/service.ts
@@ -1,6 +1,10 @@
 import { FourthwallClient, FourthwallProduct, fourthwallClient } from "./fourthwall-client"
 
-class FourthwallModuleService {
+export interface IFourthwallModuleService {
+  retrieveProducts(): Promise<FourthwallProduct[]>
+}
+
+class FourthwallModuleService implements IFourthwallModuleService {
   private readonly fourthwallClient: FourthwallClient = fourthwallClient
 
   async retrieveProducts(): Promise<FourthwallProduct[]> {
